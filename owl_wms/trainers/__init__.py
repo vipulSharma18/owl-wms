2cc32188cba8ee8f3539a313576a591c1717409b
shahbuland <smatiana1@gmail.com>
2025-06-09 21:00:02 +0000

    add causvid trainer

--- owl_wms/trainers/__init__.py
+++ owl_wms/trainers/__init__.py
@@ -3,3 +3,6 @@ from .gamerft_trainer import RFTTrainer
 def get_trainer_cls(trainer_id):
     if trainer_id == "rft":
         return RFTTrainer
+    if trainer_id == "causvid":
+        from .causvid import CausVidTrainer
+        return CausVidTrainer
