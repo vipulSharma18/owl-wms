c368f5544ea254a0522e4063a898d7acf830069a
shahbuland <smatiana1@gmail.com>
2025-06-11 14:42:10 +0000

    update shortcut config, fix rope, add reqs

--- owl_wms/nn/attn.py
+++ owl_wms/nn/attn.py
@@ -8,6 +8,7 @@ from .mlp import MLP
 import einops as eo
 
 from .modulation import AdaLN, Gate
+from .rope import FlatVideoRoPE
 
 torch.backends.cuda.enable_flash_sdp(enabled = True)
 
@@ -40,6 +41,8 @@ class Attn(nn.Module):
         self.qk_norm = QKNorm(config.d_model // config.n_heads)
         self.layer_ind = None
 
+        self.rope = FlatVideoRoPE(config)
+
         self.tokens_per_frame = config.tokens_per_frame
         self.causal = config.causal
 
@@ -62,7 +65,8 @@ class Attn(nn.Module):
 
             new_k = torch.cat([old_k, k], dim = 2).contiguous()
             new_v = torch.cat([old_v, v], dim = 2).contiguous()
-
+            
+            q,new_k = self.rope(q,new_k)
             if kv_cache.should_update:
                 kv_cache.update(new_k, new_v, self.layer_ind)
 
@@ -70,6 +74,7 @@ class Attn(nn.Module):
             x = F.scaled_dot_product_attention(q, new_k, new_v, attn_mask = mask)
             x = x[:,:,-q.shape[2]:] # Skip cached outputs (not relevant now)
         else:
+            q,k = self.rope(q,k)
             x = F.scaled_dot_product_attention(q,k,v, attn_mask = mask)
 
         x = eo.rearrange(x, 'b h n d -> b n (h d)')
