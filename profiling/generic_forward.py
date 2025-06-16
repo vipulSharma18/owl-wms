9f3c1e8658b39e05541bbea842c7b4323de43f0e
shahbuland <smatiana1@gmail.com>
2025-06-16 16:35:24 -0400

    fix

--- profiling/generic_forward.py
+++ profiling/generic_forward.py
@@ -5,14 +5,18 @@ from owl_wms.utils.owl_vae_bridge import get_decoder_only
 
 from profiling.timing import time_fn
 
-import torch._dynamo
-torch._dynamo.config.debug = True
+import torch
+
+from einops._torch_specific import allow_ops_in_compiled_graph  # requires einops>=0.6.1
+allow_ops_in_compiled_graph()
 
 wm_cfg = "configs/av.yml"
 vae_cfg = "configs/owl_vaes/cod_128x.yml"
 audio_vae_cfg = "configs/owl_vaes/cod_audio.yml"
 
 world_model = load_from_config(wm_cfg).core.bfloat16().cuda()
+img_dec = get_decoder_only(None, vae_cfg)
+audio_dec = get_decoder_only(None, audio_vae_cfg)
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -27,15 +31,29 @@ btn = torch.randint(0, 1, (1,1,11)).bfloat16().cuda()
 
 dummy = (dummy_x, dummy_audio, ts, mouse, btn)
 
-torch.compile(world_model, dynamic = False, fullgraph=True)
+world_model = torch.compile(world_model, mode='max-autotune', dynamic = False, fullgraph=True)
+img_dec = torch.compile(img_dec,mode='max-autotune', dynamic = False, fullgraph=True)
+audio_dec = torch.compile(audio_dec,mode='max-autotune', dynamic = False, fullgraph=True)
+
 res = time_fn(world_model, dummy)
 print(f"Mean: {res['mean']:.2f}ms")
 print(f"Min: {res['min']:.2f}ms")
 print(f"Max: {res['max']:.2f}ms")
 print(f"Avg FPS: {1000./res['mean']:.2f}FPS")
 
-img_dec = get_decoder_only(None, vae_cfg)
-audio_dec = get_decoder_only(None, audio_vae_cfg)
+dummy_audio_2 = torch.randn(1, 64, 120).bfloat16().cuda()
+
+res = time_fn(img_dec, dummy_x[0])
+print(f"Mean: {res['mean']:.2f}ms")
+print(f"Min: {res['min']:.2f}ms")
+print(f"Max: {res['max']:.2f}ms")
+print(f"Avg FPS: {1000./res['mean']:.2f}FPS")
+
+res = time_fn(audio_dec, dummy_audio_2)
+print(f"Mean: {res['mean']:.2f}ms")
+print(f"Min: {res['min']:.2f}ms")
+print(f"Max: {res['max']:.2f}ms")
+print(f"Avg FPS: {1000./res['mean']:.2f}FPS")
 
 """
 1B Model notes (for 1 step with KV cache)
